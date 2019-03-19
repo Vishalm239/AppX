db6e47f57a37e344a553b8ed677b0c0a0063ce3d
aksharaudupa <48679946+aksharaudupa@users.noreply.github.com>
2019-03-20 01:38:14 +0530

    Login layout

--- app/src/main/java/com/example/admin/appx/MainActivity.java
+++ app/src/main/java/com/example/admin/appx/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.admin.appx;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+           }
 }
